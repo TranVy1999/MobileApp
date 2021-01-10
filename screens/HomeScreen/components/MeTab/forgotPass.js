f3e60e01848423dc6138d953570e48d47cd9c433
vietnguyen270699 <65987730+vietnguyen270699@users.noreply.github.com>
2021-01-10 14:27:51 +0700

    navigate data and update view metab

--- screens/HomeScreen/components/MeTab/forgotPass.js
+++ screens/HomeScreen/components/MeTab/forgotPass.js
@@ -1,84 +1,86 @@
 import React from 'react';
-import { StyleSheet, Text,Alert, View,TouchableOpacity,TextInput,Dimensions} from 'react-native';
-import {  Avatar  } from "react-native-elements";
+import { StyleSheet, Text, Alert, View, TouchableOpacity, TextInput, Dimensions, ImageBackground } from 'react-native';
+import { Avatar } from "react-native-elements";
 
 const ForgotPass = (props) => {
 
 
-    return (
-      <View style={styles.container}>
-           <Avatar
-                size="xlarge"
-                rounded // làm tròn
-                source={{
-                  uri: 'https://yt3.ggpht.com/ytc/AAUvwnhsXtCE9Kpy5tX8tYzX_wOSwzkDgZA6bhb3BEo=s176-c-k-c0x00ffffff-no-rj',
-                }}/>
-                  
-                    <Text style={styles.title}>LOGO APP</Text>
-                  
-                    {/* // input email */}
-                    
-                    <TextInput placeholder="Enter the registered mail"
-                    placeholderTextColor="black"
-                    underlineColorAndroid="transparent"
-                    style={styles.txtInput}  
-                
-                    />
-               
-                    
-                    <TouchableOpacity  style={styles.btnSubmit}>
-                        <Text style={styles.txtSubmit}>Submit</Text>
-                    </TouchableOpacity>
-      </View>
-    );
-  }
+  return (
+    <View >
+      <ImageBackground style={styles.container} source={{ uri: 'https://i.pinimg.com/originals/44/8d/41/448d41e3df798cd669787b4fbd2733ea.jpg' }}>
+        <Avatar
+          size="xlarge"
+          rounded // làm tròn
+          source={{
+            uri: 'https://yt3.ggpht.com/ytc/AAUvwnhsXtCE9Kpy5tX8tYzX_wOSwzkDgZA6bhb3BEo=s176-c-k-c0x00ffffff-no-rj',
+          }} />
+
+        <Text style={styles.title}>LOGO APP</Text>
+
+        {/* // input email */}
+
+        <TextInput placeholder="Enter the registered mail"
+          placeholderTextColor="black"
+          underlineColorAndroid="transparent"
+          style={styles.txtInput}
+
+        />
+
+
+        <TouchableOpacity style={styles.btnSubmit}>
+          <Text style={styles.txtSubmit}>Submit</Text>
+        </TouchableOpacity>
+      </ImageBackground>
+    </View>
+  );
+}
 
 const DEVICE_WIDTH = Dimensions.get('window').width;
 const DEVICE_HEIGHT = Dimensions.get('window').height;
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor:  '#354f89',
+    backgroundColor: '#354f89',
     height: DEVICE_HEIGHT,
 
   },
-  title:{
-    fontSize:30,
-    color:'red'
+  title: {
+    fontSize: 30,
+    color: 'red'
   },
-  txtInput:{
+  txtInput: {
     backgroundColor: '#ffffff',
     width: DEVICE_WIDTH - 40,
-    height:"50px",
+    height: "50px",
     marginHorizontal: 20,
-    padding:8,
+    padding: 8,
     borderRadius: 20,
     color: '#000',
-    marginTop:5
+    marginTop: 5
   },
-  btnSubmit:{
-     width: DEVICE_WIDTH - 40,
-     height:"50px",
-     backgroundColor:'#5693F0',
-     padding:8,
-     borderRadius: 20,
-     marginTop:15,
-     shadowColor: "#000",
-     shadowOpacity: 0.3,
-     shadowRadius: 10,
-     shadowOffset: { width: 0, height: 0 },
- 
+  btnSubmit: {
+    width: DEVICE_WIDTH - 40,
+    height: "50px",
+    backgroundColor: '#5693F0',
+    padding: 8,
+    borderRadius: 20,
+    marginTop: 15,
+    shadowColor: "#000",
+    shadowOpacity: 0.3,
+    shadowRadius: 10,
+    shadowOffset: { width: 0, height: 0 },
+
   },
-  txtSubmit:{
-    color:'#fff',
-    textAlign:'center',
+  txtSubmit: {
+    color: '#fff',
+    textAlign: 'center',
     fontSize: '20px',
     fontWeight: '700',
   },
-  
- 
+
+
   registerTextStyle: {
     color: '#FFFFFF',
     textAlign: 'center',
@@ -87,6 +89,6 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     padding: 10,
   },
-  
+
 });
 export default ForgotPass;
--- screens/HomeScreen/components/MeTab/register.js
+++ screens/HomeScreen/components/MeTab/register.js
@@ -1,106 +1,107 @@
 import React from 'react';
-import { StyleSheet, Text,Alert, View,TouchableOpacity,TextInput,Dimensions} from 'react-native';
-import {  Avatar  } from "react-native-elements";
+import { StyleSheet, Text, Alert, View, TouchableOpacity, TextInput, Dimensions ,ImageBackground} from 'react-native';
+import { Avatar } from "react-native-elements";
 
 const Register = (props) => {
 
 
-    return (
-      <View style={styles.container}>
-           <Avatar
-                size="xlarge"
-                rounded // làm tròn
-                source={{
-                  uri: 'https://yt3.ggpht.com/ytc/AAUvwnhsXtCE9Kpy5tX8tYzX_wOSwzkDgZA6bhb3BEo=s176-c-k-c0x00ffffff-no-rj',
-                }}/>
-                    {/* // input user register */}
-                    <Text style={styles.title}>LOGO APP</Text>
-                    <TextInput placeholder="Username"
-                    placeholderTextColor="black"
-                    underlineColorAndroid="transparent"
-                    style={styles.txtInput}  
-                    //    onChangeText={(username) => this.setState({username:username})}
-                    />
-                    {/* // input email */}
-                    
-                    <TextInput placeholder="Email register"
-                    placeholderTextColor="black"
-                    underlineColorAndroid="transparent"
-                    style={styles.txtInput}  
-                    //    onChangeText={(username) => this.setState({username:username})}
-                    />
-                    {/* // input password  */}
-                    <TextInput placeholder="Password"
-                        underlineColorAndroid="transparent"
-                        placeholderTextColor="black"
-                        secureTextEntry={true}
-                        style={styles.txtInput} 
-                        //  onChangeText={(password) => this.setState({password:password})}
-                        />
-                    {/* // input password again */}
-                    <TextInput placeholder="Password again"
-                        underlineColorAndroid="transparent"
-                        placeholderTextColor="black"
-                        secureTextEntry={true}
-                        style={styles.txtInput} 
-                        //  onChangeText={(password) => this.setState({password:password})}
-                        />
-                    
-                    <TouchableOpacity  style={styles.btnLogin}>
-                        <Text style={styles.txtLogin}>Register</Text>
-                    </TouchableOpacity>
+  return (
+    <View >
+      <ImageBackground style={styles.container} source={{ uri: 'https://i.pinimg.com/originals/44/8d/41/448d41e3df798cd669787b4fbd2733ea.jpg' }}>
+        <Avatar
+          size="xlarge"
+          rounded // làm tròn
+          source={{
+            uri: 'https://yt3.ggpht.com/ytc/AAUvwnhsXtCE9Kpy5tX8tYzX_wOSwzkDgZA6bhb3BEo=s176-c-k-c0x00ffffff-no-rj',
+          }} />
+        {/* // input user register */}
+        <Text style={styles.title}>LOGO APP</Text>
+        <TextInput placeholder="Username"
+          placeholderTextColor="black"
+          underlineColorAndroid="transparent"
+          style={styles.txtInput}
+        //    onChangeText={(username) => this.setState({username:username})}
+        />
+        {/* // input email */}
 
-                
-      </View>
-    );
-  }
+        <TextInput placeholder="Email register"
+          placeholderTextColor="black"
+          underlineColorAndroid="transparent"
+          style={styles.txtInput}
+        //    onChangeText={(username) => this.setState({username:username})}
+        />
+        {/* // input password  */}
+        <TextInput placeholder="Password"
+          underlineColorAndroid="transparent"
+          placeholderTextColor="black"
+          secureTextEntry={true}
+          style={styles.txtInput}
+        //  onChangeText={(password) => this.setState({password:password})}
+        />
+        {/* // input password again */}
+        <TextInput placeholder="Password again"
+          underlineColorAndroid="transparent"
+          placeholderTextColor="black"
+          secureTextEntry={true}
+          style={styles.txtInput}
+        //  onChangeText={(password) => this.setState({password:password})}
+        />
+
+        <TouchableOpacity style={styles.btnLogin}>
+          <Text style={styles.txtLogin}>Register</Text>
+        </TouchableOpacity>
+      </ImageBackground>
+
+    </View>
+  );
+}
 
 const DEVICE_WIDTH = Dimensions.get('window').width;
 const DEVICE_HEIGHT = Dimensions.get('window').height;
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor:  '#354f89',
+    backgroundColor: '#354f89',
     height: DEVICE_HEIGHT,
 
   },
-  title:{
-    fontSize:30,
-    color:'red'
+  title: {
+    fontSize: 30,
+    color: 'red'
   },
-  txtInput:{
+  txtInput: {
     backgroundColor: '#ffffff',
     width: DEVICE_WIDTH - 40,
-    height:"50px",
+    height: "50px",
     marginHorizontal: 20,
-    padding:8,
+    padding: 8,
     borderRadius: 20,
     color: '#000',
-    marginTop:5
+    marginTop: 5
   },
-  btnLogin:{
-     width: DEVICE_WIDTH - 40,
-     height:"50px",
-     backgroundColor:'#5693F0',
-     padding:8,
-     borderRadius: 20,
-     marginTop:15,
-     shadowColor: "#000",
-     shadowOpacity: 0.3,
-     shadowRadius: 10,
-     shadowOffset: { width: 0, height: 0 },
- 
+  btnLogin: {
+    width: DEVICE_WIDTH - 40,
+    height: "50px",
+    backgroundColor: '#5693F0',
+    padding: 8,
+    borderRadius: 20,
+    marginTop: 15,
+    shadowColor: "#000",
+    shadowOpacity: 0.3,
+    shadowRadius: 10,
+    shadowOffset: { width: 0, height: 0 },
+
   },
-  txtLogin:{
-    color:'#fff',
-    textAlign:'center',
+  txtLogin: {
+    color: '#fff',
+    textAlign: 'center',
     fontSize: '20px',
     fontWeight: '700',
   },
-  
- 
+
+
   registerTextStyle: {
     color: '#FFFFFF',
     textAlign: 'center',
@@ -109,6 +110,6 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     padding: 10,
   },
-  
+
 });
 export default Register;
